8a35f27585ffc707688013585e11219ca5d4f848
yoonseo <yo26on@soongsil.ac.kr>
2022-06-17 22:33:13 +0900

    Landmarks (#1)

---
+++ apple-tutorials/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김윤서 on 2022/06/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
